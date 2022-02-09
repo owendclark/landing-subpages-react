926de8439b79b3c5dadcf0934451248e052add43
Lyman Starmer <dummy@icloud.com>
2022-02-08 19:24:27 -0500

    Added last two cards on about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -36,36 +36,64 @@ const AboutPage = () => {
               Increasing collaboration one discussion at a time…
             </h3>
             <p className={classes.contentText}>
-              <span className={classes.firstLetterContent}>I</span>magine if dad and
-              grandpa actually agreed, or understood each other when conversing
-              at Thanksgiving…that would be the life, right? Well…what if they
-              actually could? What if there was a solution that not only fixed
-              micro issues, but macro issues too. Well… there is! Perspective
-              offers individuals to express how they feel and converse with the
-              other side in a healthy and structured way…you never hear that
-              nowadays…Find new Perspectives and value human dialogue, right
-              here on your device.
+              <span className={classes.firstLetterContent}>I</span>magine if dad
+              and grandpa actually agreed, or understood each other when
+              conversing at Thanksgiving…that would be the life, right?
+              Well…what if they actually could? What if there was a solution
+              that not only fixed micro issues, but macro issues too. Well…
+              there is! Perspective offers individuals to express how they feel
+              and converse with the other side in a healthy and structured
+              way…you never hear that nowadays…Find new Perspectives and value
+              human dialogue, right here on your device.
             </p>
           </div>
         </div>
-        <div>
-          <div>
-            {/* Title */}
-            {/* Content */}
+        <div className={classes.cardContainerLeftJustify}>
+          <div className={classes.contentContainer}>
+            <h3 className={classes.subjectHeading}>
+              Putting the “u” back in user…
+            </h3>
+            <p className={classes.contentText}>
+              <span className={classes.firstLetterContent}>B</span>y bolstering
+              data protection and altering the way patrons view advertisements
+              on Perspective, we give the autonomy back to the user and
+              strengthen the relationship between humans and tech. Who really
+              wants to be tracked for ads…? I know we don’t.
+            </p>
           </div>
-          <div>
+          <div className={classes.imageContainer}>
             {/* Background Curves */}
-            {/* Image */}
+            <img
+              src="assets/images/logo.png"
+              alt="logo"
+              width={500}
+              height={500}
+              className={classes.cardImage}
+            />
           </div>
         </div>
-        <div>
-          <div>
+        <div className={classes.cardContainerLeftJustify}>
+          <div className={classes.imageContainer}>
             {/* Background Curves */}
-            {/* Image */}
+            <img
+              src="assets/images/logo.png"
+              alt="logo"
+              width={500}
+              height={500}
+              className={classes.cardImage}
+            />
           </div>
-          <div>
-            {/* Title */}
-            {/* Content */}
+          <div className={classes.contentContainer}>
+            <h3 className={classes.subjectHeading}>
+              Understanding the unthinkable, breaking down the unbearable…
+            </h3>
+            <p className={classes.contentText}>
+            <span className={classes.firstLetterContent}>T</span>rue education and learning comes from the understanding of
+              different Perspectives, whether they are “unthinkable” to you, or
+              just right. By learning to collaborate and converse with
+              individuals of different Perspectives, yourself and the world
+              around you will begin to open up with new opportunities and ideas.
+            </p>
           </div>
         </div>
       </main>
