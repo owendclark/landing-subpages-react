1beac26723748d53f3499de37df67e573562a59a
owendclark <65785581+owendclark@users.noreply.github.com>
2022-02-10 21:26:18 -0500

    Finished About Page Styling

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -15,9 +15,6 @@ const AboutPage = () => {
             height={100}
           />
         </div>
-        <div className={classes.headerTextContainer}>
-          <h1 className={classes.aboutText}>About</h1>
-        </div>
       </header>
       <main className={classes.mainContainer}>
         <div
@@ -52,7 +49,6 @@ const AboutPage = () => {
             </p>
           </div>
         </div>
-        <div className={classes.divider1}></div>
         <div
           className={`${classes.cardContainerLeftJustify} ${classes.secondCard}`}
         >
@@ -79,8 +75,17 @@ const AboutPage = () => {
               />
             </div>
           </div>
+          <div className={classes.waveDivider1}>
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+            >
+              <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"></path>
+            </svg>
+          </div>
         </div>
-        <div className={classes.divider2}></div>
         <div
           className={`${classes.cardContainerLeftJustify} ${classes.thirdCard}`}
         >
@@ -107,6 +112,16 @@ const AboutPage = () => {
               different Perspectives, yourself and the world around you will
               begin to open up with new opportunities and ideas.
             </p>
+          </div>
+          <div className={classes.waveDivider2}>
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+            >
+              <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"></path>
+            </svg>
           </div>
         </div>
       </main>
